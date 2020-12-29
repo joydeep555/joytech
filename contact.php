fd8e578e059de685eef2712979b2febf7bb08cc1
joydeep555 <60765429+joydeep555@users.noreply.github.com>
2020-12-29 14:55:55 +0530

    Update contact.php

--- contact.php
+++ contact.php
@@ -3,14 +3,10 @@ $name = $_POST['name']
 $email = $_POST['email']
 $message = $_POST['message']
 
-if (empty($name) or (empty($email) or (empty($message))
-{
-	echo "Please fill all the fields";
-}
-else
-{
-	mail("joydeep5.asn@gmail.com", "Personal Portfolio Website Message", $message, "From: $name < $email");
-	echo"<script type='text/javascript'>alert('Your Message have been delivered successfully');
-	window.history.log(-1);
-	</script>";
-}
+$formcontent="From: $name \n Message: $message";
+$mailTo = "joydeep5.asn@gmail.com";
+$subject = "Contact From Submission";
+$mailheader = "From: $email \r\n";
+mail($mailTo, $subject, $formcontent, $mailheader) or die("Error!");
+echo "Thank You!";
+?>
